d102bc6505988015a3b0544600db620d286097c1
jfang9493 <jfang9493@bths.edu>
2018-11-20 02:33:31 -0500

    hehehs

--- src/InPlaceSorts.java
+++ src/InPlaceSorts.java
@@ -2,9 +2,9 @@ public class InPlaceSorts {
     public static void insertionSort(int[] list1)
     {
         int x;
-        for (int i = 0; i < list1.length - 1; i++)
+        for (int i = 1; i < list1.length; i++)
         {
-            for (int j = i; j >= 0; j--)
+            for (int j = i; j - 1 >= 0; j--)
             {
                 if (list1[j-1] > list1[j])
                 {
@@ -12,6 +12,7 @@ public class InPlaceSorts {
                     list1[j-1] = list1[j];
                     list1[j] = x;
                 }
+                else j = 0;
             }
         }
     }
@@ -21,11 +22,11 @@ public class InPlaceSorts {
         double swapPos = 0;
         double currMin = 0;
         double x;
-        for (double i = 0; i < list1.length - 1; i++)
+        for (int i = 0; i < list1.length - 1; i++)
         {
             swapPos = i;
             currMin = list1[i];
-            for (double j = i + 1; j < list1.length - 1; j++)
+            for (int j = i; j < list1.length - 1; j++)
             {
                 if (currMin > list1[j])
                 {
@@ -33,8 +34,8 @@ public class InPlaceSorts {
                     currMin = list1[j];
                 }
             }
-            x = i;
-            i = swapPos;
+            x = list1[i];
+            list1[i] = swapPos;
             swapPos = x;
         }
     }
@@ -53,8 +54,52 @@ public class InPlaceSorts {
                     x = list1[i];
                     list1[i] = list1[i+1];
                     list1[i+1] = x;
+                    swap++;
                 }
             }
         }
+    }
+
+    public int[] randomIntArr(int count)
+    {
+        int[] arr = new int[count];
+        for (int i = 0; i < arr.length; i++)
+        {
+            arr[i] = (int)(Math.random()*100);
+            if (i == arr.length-1) System.out.println(arr[i]);
+            else System.out.print(arr[i] + ",");
+        }
+        return arr;
+    }
+
+    public double[] randomDoubleArr(int count)
+    {
+        double[] arr = new double[count];
+        for (int i = 0; i < arr.length; i++)
+        {
+            arr[i] = (Math.random()*100);
+            //if (i == arr.length-1) System.out.println(arr[i]);
+            //else System.out.print(arr[i] + ",");
+        }
+        return arr;
+    }
+
+    public String[] randomStringArr(int num, int length)
+    {
+        String[] arr = new String[num];
+        while (num > 0)
+        {
+            int i = 0;
+            String s = "";
+            while (i < length)
+            {
+                char c = (char)((Math.random()*26)+97);
+                s = s + c;
+                i++;
+            }
+            num--;
+            arr[num] = s;
+        }
+        return arr;
     }
 }
--- src/Runner.java
+++ src/Runner.java
@@ -2,6 +2,41 @@ public class Runner {
     public static void main(String[] args)
     {
         InPlaceSorts numbers = new InPlaceSorts();
+
+        int[] intArr = numbers.randomIntArr(10);
+        double[] doubleArr = numbers.randomDoubleArr(10);
+        String[] stringArr = numbers.randomStringArr(10,3);
+
+
+        long start = System.nanoTime();
+        numbers.insertionSort(intArr);
+        long elapsed = System.nanoTime() - start;
+        for (int i = 0; i < intArr.length; i++)
+        {
+            if (i == intArr.length-1) System.out.println(intArr[i]);
+            else System.out.print(intArr[i] + ",");
+        }
+        System.out.println("Insertion Sort took: " + elapsed + " nanoseconds." + "\n");
+
+        long start1 = System.nanoTime();
+        numbers.selectionSort(doubleArr);
+        long elapsed1 = System.nanoTime() - start1;
+        for (int i = 0; i < doubleArr.length; i++)
+        {
+            if ( i == doubleArr.length-1) System.out.println(doubleArr[i]);
+            else System.out.print(doubleArr[i] + ",");
+        }
+        System.out.println("Selection Sort took: " + elapsed1 + " nanoseconds." + "\n");
+
+        long start2 = System.nanoTime();
+        numbers.bubbleSort(stringArr);
+        long elapsed2 = System.nanoTime() - start1;
+        for (int i = 0; i < stringArr.length; i++)
+        {
+            if ( i == stringArr.length-1) System.out.println(stringArr[i]);
+            else System.out.print(stringArr[i] + ",");
+        }
+        System.out.println("Bubble Sort took: " + elapsed2 + " nanoseconds." + "\n");
     }
 
 }
